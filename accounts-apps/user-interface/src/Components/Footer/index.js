8ca98c63c9dbd742410262f6bd1bc19a54520cb2
Deepankar Agrawal <deepankar_agrawal@external.mckinsey.com>
2018-09-28 19:50:20 +0530

    updated names

--- accounts-apps/user-interface/src/Components/Footer/index.js
+++ accounts-apps/user-interface/src/Components/Footer/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
+import { getMessage } from '../../utils';
 
-const Footer = () => <div className="footer">Footer</div>;
+const Footer = ({ universityName }) => (
+  <div className="footer">{universityName}</div>
+);
 
+Footer.propTypes = {
+  universityName: PropTypes.string,
+};
+Footer.defaultProps = {
+  universityName: getMessage('layout', 'universityName'),
+};
 export default Footer;
--- accounts-apps/user-interface/src/Components/Header/index.js
+++ accounts-apps/user-interface/src/Components/Header/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { getMessage } from '../../utils';
 import './style.css';
 
-const Header = () => <div className="header">Header</div>;
+const Header = ({ appName }) => <div className="header">{appName}</div>;
 
+Header.propTypes = {
+  appName: PropTypes.string,
+};
+
+Header.defaultProps = {
+  appName: getMessage('layout', 'appName'),
+};
 export default Header;
